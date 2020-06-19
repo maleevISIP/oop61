c5ed159a438b61bdfb989b7d7294cbc90ecf3934
Egor Maleev <egor.maleev@list.ru>
2020-06-19 19:17:08 +0500

    egorka

---
+++ oop61/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop61
+{
+    class Nums
+    {
+        private int NumsOne;
+        private int NumsTwo;
+
+        public Nums()
+        {
+
+        }
+
+        public Nums(int NumsOnen)
+        {
+            NumsOne = NumsOnen;
+        }
+
+        public Nums(int NumsOnen, int NumsTwon)
+        {
+            NumsOne = NumsOnen;
+            NumsTwo = NumsTwon;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"{NumsOne} {NumsTwo}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nums num = new Nums();
+            num.Display();
+            Nums NumsOnen = new Nums(1);
+            NumsOnen.Display();
+            Nums NumsTwon = new Nums(2, 3);
+            NumsTwon.Display();
+
+            Console.ReadLine();
+        }
+    }
+}
